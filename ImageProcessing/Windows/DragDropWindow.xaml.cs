8e353af7a5c8d03948a41afe1d776f5aae0a30bf
Casper Bastholm <bastholm.casper@gmail.com>
2020-06-12 07:35:04 +0200

    Changed drag drop button border brush.

--- ImageProcessing/Windows/DragDropWindow.xaml.cs
+++ ImageProcessing/Windows/DragDropWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using MahApps.Metro.Controls;
-using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Runtime.InteropServices;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ImageProcessing.Windows
 {
@@ -30,11 +19,11 @@ namespace ImageProcessing.Windows
                 Button newButton = new Button
                 {
                     BorderThickness = new Thickness(2),
-                    BorderBrush = button.BorderBrush,
                     Content = button.Content,
                     IsHitTestVisible = false,
                     Opacity = .7
                 };
+                newButton.SetResourceReference(BorderBrushProperty, "MahApps.Brushes.Accent");
 
                 stackPanelButtons.Children.Add(newButton);
             }
