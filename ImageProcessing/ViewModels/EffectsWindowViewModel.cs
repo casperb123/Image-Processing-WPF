505fa4e4fd97000b0f3aa92e6e26f1faef14f9cb
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2020-06-18 11:38:53 +0200

    Removed unused usings.

--- ImageProcessing/ViewModels/EffectsWindowViewModel.cs
+++ ImageProcessing/ViewModels/EffectsWindowViewModel.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
